163283e1125cb0a6df0222fa1063538afe98c5e0
Micheal Koblinski <stevenkoblinskip@gmail.com>
2020-01-10 04:28:43 -0500

    added babel plugin for class properties syntax

--- src/components/IndecisionApp.js
+++ src/components/IndecisionApp.js
@@ -5,15 +5,33 @@ import Action from './Action'
 import Header from './Header'
 
 class IndecisionApp extends React.Component {
-  constructor(props) {
-    super(props)
-    this.handleDeleteOptions = this.handleDeleteOptions.bind(this)
-    this.handlePick = this.handlePick.bind(this)
-    this.handleAddOption = this.handleAddOption.bind(this)
-    this.handleDeleteOption = this.handleDeleteOption.bind(this)
-    this.state = {
-      options: []
+  state = {
+    options: []
+  }
+
+  handleDeleteOption = (optionToRemove) => {
+    this.setState((prevState) => ({
+      options: prevState.options.filter((option) => optionToRemove !== option)
+    }))
+  }
+
+  handleDeleteOptions = () => {
+    this.setState(() => ({ options: [] }))
+  }
+
+  handlePick = () => {
+    const randomNum = Math.floor(Math.random() * this.state.options.length);
+    const option = this.state.options[randomNum]
+    alert(option)
+  }
+
+  handleAddOption = (option) => {
+    if (!option) {
+      return 'Enter valid value to add item'
+    } else if (this.state.options.indexOf(option) > -1) {
+      return 'This option already exists'
     }
+    this.setState((prevState) => ({ options: prevState.options.concat(option)}))
   }
 
   componentDidMount() {
@@ -35,31 +53,6 @@ class IndecisionApp extends React.Component {
       const json = JSON.stringify(this.state.options)
       localStorage.setItem('options', json)
     }
-  }
-
-  handleDeleteOption(optionToRemove) {
-    this.setState((prevState) => ({
-      options: prevState.options.filter((option) => optionToRemove !== option)
-    }))
-  }
-
-  handleDeleteOptions() {
-    this.setState(() => ({ options: [] }))
-  }
-
-  handlePick() {
-    const randomNum = Math.floor(Math.random() * this.state.options.length);
-    const option = this.state.options[randomNum]
-    alert(option)
-  }
-
-  handleAddOption(option) {
-    if (!option) {
-      return 'Enter valid value to add item'
-    } else if (this.state.options.indexOf(option) > -1) {
-      return 'This option already exists'
-    }
-    this.setState((prevState) => ({ options: prevState.options.concat(option)}))
   }
 
   render() {
